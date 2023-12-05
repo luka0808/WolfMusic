4c70ccf0b9257bf19ecd94bdcd4c31e0c793635c
luka0808 <isabelledezmaromero@gmail.com>
2023-12-05 15:19:44 -0300

    Login Pagina Ventas

--- AdmProducto/main.js
+++ AdmProducto/main.js
@@ -1,9 +1,8 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-light">
 <div class="container">
-<a class="navbar-brand" href="productos.html">CRUD</a>
-
-
+<a class="navbar-brand" href="../Pagina Ventas/index.html">Volver a la Tienda</a>
 
 </div>
-</nav>`
+</nav>
+<h1>Administracion de la tienda</h1>
