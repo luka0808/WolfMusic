b32551e5cf21726e60eba9462d8e6f52d8c66e48
luka0808 <isabelledezmaromero@gmail.com>
2023-12-05 18:46:23 -0300

    Modificacion CRODS

--- AdmProducto/main.js
+++ AdmProducto/main.js
@@ -1,9 +1,10 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-light">
 <div class="container">
-<a class="navbar-brand" href="productos.html">CRUD</a>
+<a class="navbar-brand" href="../Pagina Ventas/index.html">Volver a la tienda</a>
 
 
 
 </div>
 </nav>`
+
