7ea657b9d9bf09c0acebdf33047acfcaf891ff59
luka0808 <isabelledezmaromero@gmail.com>
2023-12-05 18:35:59 -0300

    WolfCarteleraV2

--- Pagina Ventas/AdmProducto/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-light bg-light">
-<div class="container">
-<a class="navbar-brand" href="../Pagina Ventas/index.html">Volver a la Tienda</a>
-
-</div>
-</nav>
-<h1>Administracion de la tienda</h1>
